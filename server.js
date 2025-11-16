d06021023eeba26922de4741c8cc2a9724b8dc08
bitflicker64 <himnshuverma10152006@gmail.com>
2025-11-16 21:20:30 +0530

    Fix join screen: correct element IDs and Socket.IO client path

--- server.js
+++ server.js
@@ -4,7 +4,24 @@ const path = require("path")
 const app = express();
 const server = require("http").createServer(app);
 
+const io = require("socket.io")(server);
+
 app.use(express.static(path.join(__dirname+"/public")));
+
+io.on("connection", function(socket){
+    socket.on("newuser",function(username){
+        // just a joke 
+        socket.broadcast.emit("update",username +" joined the terrorist's (AL-Qa'ida(AQ))");
+    });
+    socket.on("exituser",function(username){
+        // just a joke 
+        socket.broadcast.emit("update",username +" Left the terrorist's (AL-Qa'ida(AQ))");
+    });
+    socket.on("chat",function(message){
+        // just a joke 
+        socket.broadcast.emit("chat",message);
+    });
+});
 
 server.listen(5001);
 
